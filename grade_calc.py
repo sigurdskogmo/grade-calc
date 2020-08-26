77d1ca55a2e496db3ad334bdf281633c637f71e1
sigurdskogmo <sigurd.skogmo@gmail.com>
2020-08-26 12:46:57 +0200

    Input control and removal of unused list

--- grade_calc.py
+++ grade_calc.py
@@ -1,34 +1,5 @@
 # This program is only intended to give an approximate and does not necessarily represent the teacher's grading.
 
-grades_full = [
-            [1, 21.67, "1"],
-            [21.67, 23.33, "1+"],
-            [23.33, 25, "1/2"],
-            [25, 27.67, "2/1"],
-            [27.67, 28.33, "2-"],
-            [28.33, 38.33, "2"],
-            [38.33, 40, "2+"],
-            [40, 41.67, "2/3"],
-            [41.67, 43.33, "3/2"],
-            [43.33, 45, "3-"],
-            [45, 55, "3"],
-            [55, 56.67, "3+"],
-            [56.67, 58.33, "3/4"],
-            [58.33, 60, "4/3"],
-            [60, 61.67, "4-"],
-            [61.67, 71.67, "4"],
-            [71.67, 73.33, "4+"],
-            [73.33, 75, "4/5"],
-            [75, 76.67, "5/4"],
-            [76.67, 78.33, "5-"],
-            [78.33, 88.33, "5"],
-            [88.33, 90, "5+"],
-            [90, 91.67, "5/6"],
-            [91.67, 93.33, "6/5"],
-            [93.33, 95, "6-"],
-            [95, 100, "6"]
-]
-
 grades = [
     [1, "1"],
     [21.67, "1+"],
@@ -58,15 +29,22 @@ grades = [
     [95, "6"],
     [100, "NO GRADE"]
 ]
+def get_value(message):
+    while True:
+        user_input = input(message)
+        try:
+            val = int(user_input)
+            if val >= 0:
+                break
+            else:
+                print("Points can't be negative, try again")
+        except ValueError:
+            print("Not an integer! Try again.")
+    return val
 
 def calc_grade():
-    final_grade = "NaN"
-
-    print("How many points did you get?")
-    pts = input('> ')
-
-    print("How many points were achievable?")
-    max_pts = input('> ')
+    pts = get_value("How many points did you get?\n> ")
+    max_pts = get_value("How many points were achievable?\n> ")
 
     score_dec = float(pts) / float(max_pts)
     score = round(score_dec, 4) * 100
@@ -79,7 +57,7 @@ def calc_grade():
             i += 1
         else:
             final_score = grades[i - 1][1]
-            print("You will most likely be graded " + final_score + ".\n\n")
+            print("You will most likely be graded " + final_score + ".\n")
             break
     calc_grade()
 
