5fe6af95b82105efbe79870635ac3b362c3f2d01
sigurdskogmo <sigurd.skogmo@gmail.com>
2020-08-26 22:12:25 +0200

    Make sure user enters a big enough integer

--- grade_calc.py
+++ grade_calc.py
@@ -1,50 +1,23 @@
 # This program is only intended to give an approximate and does not necessarily represent the teacher's grading.
 
-grades = [
-    [1, "1"],
-    [21.67, "1+"],
-    [23.33, "1/2"],
-    [25, "2/1"],
-    [27.67, "2-"],
-    [28.33, "2"],
-    [38.33, "2+"],
-    [40, "2/3"],
-    [41.67, "3/2"],
-    [43.33, "3-"],
-    [45, "3"],
-    [55, "3+"],
-    [56.67, "3/4"],
-    [58.33, "4/3"],
-    [60, "4-"],
-    [61.67, "4"],
-    [71.67, "4+"],
-    [73.33, "4/5"],
-    [75, "5/4"],
-    [76.67, "5-"],
-    [78.33, "5"],
-    [88.33, "5+"],
-    [90, "5/6"],
-    [91.67, "6/5"],
-    [93.33, "6-"],
-    [95, "6"],
-    [100, "NO GRADE"]
-]
-def get_value(message):
+from grade_scale import grades
+
+def get_value(message, min_val):
     while True:
         user_input = input(message)
         try:
             val = int(user_input)
-            if val >= 0:
+            if val >= min_val:
                 break
             else:
-                print("Points can't be negative, try again")
+                print(f"Must be higher than {min_val}, try again")
         except ValueError:
             print("Not an integer! Try again.")
     return val
 
-def calc_grade():
-    pts = get_value("How many points did you get?\n> ")
-    max_pts = get_value("How many points were achievable?\n> ")
+def calc_grade():   
+    pts = get_value("How many points did you get?\n> ", 0)
+    max_pts = get_value("How many points were achievable?\n> ", 1)
 
     score_dec = float(pts) / float(max_pts)
     score = round(score_dec, 4) * 100
