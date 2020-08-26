bd89517ce485d1559a62a73bf2abadd8b3c0f5b5
sigurdskogmo <sigurd.skogmo@gmail.com>
2020-08-26 22:49:55 +0200

    fix logical error and making sure user input is correct

--- grade_calc.py
+++ grade_calc.py
@@ -6,7 +6,7 @@ def get_value(message, min_val):
     while True:
         user_input = input(message)
         try:
-            val = int(user_input)
+            val = float(user_input)
             if val >= min_val:
                 break
             else:
@@ -15,9 +15,17 @@ def get_value(message, min_val):
             print("Not an integer! Try again.")
     return val
 
-def calc_grade():   
-    pts = get_value("How many points did you get?\n> ", 0)
-    max_pts = get_value("How many points were achievable?\n> ", 1)
+def calc_grade():
+    while True:
+        pts = get_value("How many points did you get?\n> ", 0)
+        max_pts = get_value("How many points were achievable?\n> ", 1)
+        try:
+            if float(pts) <= float(max_pts):
+                break   
+            else:
+                print("Achievable points must be higher or than points achieved")
+        except ValueError:
+                print("Wrong input data")
 
     score_dec = float(pts) / float(max_pts)
     score = round(score_dec, 4) * 100
@@ -25,13 +33,17 @@ def calc_grade():
     print(f"Your score was: {score}%.")
 
     i = 0
+    final_score = ""
     while i < len(grades):
-        if score > grades[i][0]:
-            i += 1
-        else:
-            final_score = grades[i - 1][1]
-            print("You will most likely be graded " + final_score + ".\n")
+        if score == grades[i][0]:
+            final_score = grades[i][1]
             break
+        elif score > grades[i][0] and score < grades[i + 1][0]:
+            final_score = grades[i][1]
+            break
+        else:
+            i += 1
+    print(f"You will most likely be graded {final_score}")
     calc_grade()
 
 calc_grade()
